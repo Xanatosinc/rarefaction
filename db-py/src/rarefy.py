1446b304b1ad63e41fcd64ccc7e430a1513b0900
David Swartout <david.swartout@gmail.com>
2019-12-02 23:13:04 -0800

    db-py: Draft rarefy.py

--- db-py/src/rarefy.py
+++ db-py/src/rarefy.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env/python
 
+# rarefy.py
 # Pull data from database and calculate coverage per ecotype per station per gene.
+# Stations that have a summed read_length less than the defined THRESHOLD value are ignored.
+
+# Number of gene_read records per station per ecotype required for consideration
+STATION_READ_MIN = 10000
+
+# Number of gene_reads randomly sampled per station per ecotype
+#DEPTHS = (10000, 25000, 50000, 75000, 100000)
+DEPTHS = [10000]
 
 from mysql.connector import connect
 import os, pandas as pd, sys
-import dask.dataframe as ddf
 
 if len(sys.argv) != 2:
     exit('Usage: rarefy.py ECOTYPE')
@@ -31,31 +39,76 @@ for ecotypeId, ecotypeName in cur.fetchall():
 if ECOTYPE not in ecotypes:
     exit('Ecotype ' + ECOTYPE + ' not found in database. Ecotypes found: ' + ecotypes.keys().join(', '))
 
+# Fetch genes
+cur.execute('SELECT DISTINCT gene_id FROM gene_reads')
+genes = cur.fetchall()
+
+# Fetch data
+print('fetching joined gene_reads data')
+df = pd.read_sql('''
+    SELECT gr.gene_id, gr.read_number, s.name station_name, gr.read_length FROM gene_reads gr
+    LEFT JOIN stations s ON s.id = gr.station_id
+    LEFT JOIN contigs c ON c.id = gr.contig_id
+    LEFT JOIN ecotypes e ON e.id = c.ecotype_id
+    WHERE 1=1
+        AND e.name = '%s'
+    ''' % (ECOTYPE),
+    con=con
+)
+
+# Length of genes based on reference sequence
+geneLengths = pd.read_sql('SELECT gene_id, length FROM gene_ref_lengths', con=con).set_index('gene_id')
+print(geneLengths)
+
 # Fetch stations
 stations = {} # name => id
 cur.execute('SELECT id, name FROM stations')
+
+print('Iterating through stations')
+
+# Output for this script: genes x stations
+outputTables = {}
+for sampleDepth in DEPTHS:
+    outputTables[sampleDepth] = pd.DataFrame(index=geneLengths.index)
+
 for stationId, stationName in cur.fetchall():
     stations[stationName] = stationId
     print(stationName)
 
-#    df = pd.read_sql('''
-#        SELECT gr.gene_id, gr.read_number, s.name, gr.read_length FROM gene_reads gr
-#        LEFT JOIN stations s ON s.id = gr.station_id
-#        LEFT JOIN contigs c ON c.id = gr.contig_id
-#        LEFT JOIN ecotypes e ON e.id = c.ecotype_id
-#        WHERE 1=1
-#            AND e.name = '%s'
-#            AND s.name = '%s'
-#        ''' % (ECOTYPE, stationName),
-#        con=con
-#    )
-    MYSQL_DB=os.getenv('MYSQL_DB')
-    MYSQL_USER=os.getenv('MYSQL_USER')
-    MYSQL_PASS=os.getenv('MYSQL_PASS')
-    MYSQL_PORT=os.getenv('MYSQL_PORT')
 
-    ddf_con_string = 'mysql://'+MYSQL_USER+':'+MYSQL_PASS+'@0.0.0.0:'+MYSQL_PORT+'/'+MYSQL_DB
-    df = ddf.read_sql_table('gene_reads', ddf_con_string, index_col='id')
-    print(df)
-    exit()
+    stationDf = df[df.station_name == stationName]
 
+    stationReadCount = len(stationDf.index)
+
+    if (stationReadCount > STATION_READ_MIN):
+        for sampleDepth in DEPTHS:
+
+            ## TODO: If stationReadCount < sampleDepth, zerofill the entire station
+            if stationReadCount < sampleDepth:
+                outputTables[sampleDepth][stationName] = 0
+                break
+
+            # Random sampling of this station's gene_reads
+            sampleDf = stationDf.sample(n = sampleDepth)
+
+            # Sums of the read_length for each gene for this station
+            geneReadLengthSums = sampleDf.groupby('gene_id')['read_length'].sum().reset_index(name = 'sum').set_index('gene_id')
+
+            # The number of gene_reads for this gene in this station
+            uniqueGeneCount = sampleDf['gene_id'].nunique()
+
+            # Join sums of read lengths with gene reference lengths
+            grls = geneReadLengthSums.join(geneLengths, how='inner')
+            outputTables[sampleDepth][stationName] = grls['sum'] / grls['length']
+            grls['coverage'] = grls['sum'] / grls['length']
+            print('summary data for ' + stationName)
+            print(outputTables[sampleDepth].dropna(subset=[stationName]))
+
+    else:
+        outputTables[sampleDepth][stationName] = 0
+
+for sampleDepth in DEPTHS:
+    fileOutName = OUTPUT_DIR + '/' + ECOTYPE + '_' + str(sampleDepth) + '.tsv'
+    print('file out would be ' + fileOutName)
+#    fileOut = open('w', fileOutName)
+#    outputTables[sampleDepth].to_csv(fileOut, sep='\t')
