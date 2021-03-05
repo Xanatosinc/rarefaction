01dc0dc98ef69f9de2c65bac91a3b24daaee940c
Alyse Larkin <aalarkin@gmail.com>
2021-03-04 16:41:54 -0800

    Fixed config bug, removed contig table from create-tables sql

--- db-init/init-rarefaction-db.sh
+++ db-init/init-rarefaction-db.sh
@@ -7,23 +7,23 @@ else
 	exit 1
 fi
 
-if [ -z "${CONTAINER+x}" ]; then
+if [ -z "${CONTAINER_NAME+x}" ]; then
 	echo "Container name not set."
 	exit 1
 fi
 
 echo "Creating schema, granting privileges."
-docker exec -it ${CONTAINER} mysql -u 'root' -p"${MYSQL_ROOT_PASS}" -e " \
+docker exec -it ${CONTAINER_NAME} mysql -u 'root' -p"${MYSQL_ROOT_PASS}" -e " \
 	CREATE DATABASE IF NOT EXISTS ${MYSQL_DB}; \
 	GRANT ALL ON ${MYSQL_DB}.* TO '${MYSQL_USER}'@'%'; \
 	flush privileges;"
 
-docker exec -it ${CONTAINER} mysql -u 'root' -p"${MYSQL_ROOT_PASS}" -e " \
+docker exec -it ${CONTAINER_NAME} mysql -u 'root' -p"${MYSQL_ROOT_PASS}" -e " \
 	GRANT SELECT ON *.* TO '${MYSQL_USER}'@'%'; \
 	flush privileges;"
 
 echo "Creating tables."
-docker cp ./sql/create-tables.sql ${CONTAINER}:/create-tables.sql
-docker exec -it ${CONTAINER} bin/bash -c "cat /create-tables.sql | sed 's/#MYSQL_DB#/${MYSQL_DB}/' | mysql -u"${MYSQL_USER}" -p"${MYSQL_PASS}
+docker cp ./sql/create-tables.sql ${CONTAINER_NAME}:/create-tables.sql
+docker exec -it ${CONTAINER_NAME} bin/bash -c "cat /create-tables.sql | sed 's/#MYSQL_DB#/${MYSQL_DB}/' | mysql -u"${MYSQL_USER}" -p"${MYSQL_PASS}
 
 echo "Done."
